6d040d66a8e6a1acc02fc65c3897b14d71ab2c2a
Rachel Moon <hj.rachelmoon@gmail.com>
2022-05-31 13:59:34 -0700

    In progress

---
+++ static/js/sharkwords.js
@@ -0,0 +1,78 @@
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+const WORDS = [
+  'strawberry',
+  'orange',
+  'apple',
+  'banana',
+  'pineapple',
+  'kiwi',
+  'peach',
+  'pecan',
+  'eggplant',
+  'durian',
+  'peanut',
+  'chocolate',
+];
+
+const numWrong = 0;
+
+// Loop over the chars in `word` and create divs.
+// The divs should be appended to the section with id="word-container".
+const createDivsForChars = (word) => {
+  
+  for (const letter of word) {
+    document.querySelector('#word-container').insertAdjacentHTML('beforeend', '<div class="letter-box ${letter}"></div>');
+  }
+};
+
+// Loop over each letter in the alphabet and generate a button for each letter
+// The buttons should be appended to the section with id="letter-buttons"
+const generateLetterButtons = () => {
+  for (const alph of ALPHABET) {
+    document.querySelector('#letter-buttons').insertAdjacentHTML('beforeend', `<button class="letter-buttons" >${alph}</button>`);
+  }
+};
+
+// Set the `disabled` property of `buttonEl` to `true.
+//
+// `buttonEl` is an `HTMLElement` object.
+//
+const disableLetterButton = (buttonEl) => {
+  buttonEl.disabled = true
+};
+
+// This is a helper function we will use in the future
+// It should return `true` if `letter` is in the word
+// For now, you should test it out to make sure it works
+
+const isLetterInWord = (letter) => {
+  const letterDiv = document.querySelector(`.${letter}`);
+  // let allDivs = createDivsForChars(WORDS);
+  // for (const oneDiv of allDivs) {
+  //   if (letterDiv == allDivs) {
+  //     return true;
+  //   }
+  // }
+};
+
+// This is like if __name__ == '__main__' in Python
+// It will be called when the file is run (because
+// we call the function on line 66)
+(function startGame() {
+  // For now, we'll hardcode the word that the user has to guess
+  // You can change this to choose a random word from WORDS once you
+  // finish this lab but we hard code it so we know what the word is
+  // and can tell if things look correct for this word
+
+  const word = 'hello';
+
+  // call the function that makes an empty line for each letter in the word
+  createDivsForChars(word);
+
+  // call the function that makes a button for each letter in the alphabet
+  generateLetterButtons()
+
+  // in the next lab, you will be adding functionality to handle when
+  // someone clicks on a letter
+})();
